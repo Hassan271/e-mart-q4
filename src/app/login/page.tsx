e129d4eb2728fc4483b9329f722c12bea4fd941d
Hassan Farooq <63926207+Hassan271@users.noreply.github.com>
2024-11-12 00:12:00 +0500

    commit-2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,25 +17,26 @@ function LoginPage() {
 
     // Login Starts  =================================================
 
-    <div className="max-w-[1166px] my-2 mx-auto bg-black flex flex-wrap rounded-lg justify-between items-center">
+  <div className="max-w-[1305px] max-h-[780px] my-2 mx-auto bg-transparent flex flex-wrap rounded-lg justify-between items-center border  border-zinc-500 ">
   {/* <!-- Left Section --> */}
-  <div className="flex items-center shadow-md rounded-lg overflow-hidden justify-center w-[20rem] mx-6 md:w-auto my-4 basis-[50%]">
+  <div className=" bg-[#CBE4E8] flex items-center shadow-lg rounded-lg overflow-hidden justify-center max-w-[805px] max-h-[780px]  mx-6 md:w-auto my-4 basis-[50%]">
     <div className="text-muted hidden md:flex justify-center items-center flex-col h-dvh w-dvh">
       <h3 className="text-2xl font-semibold">Welcome!</h3>
-      <p>Enter your personal details for login</p>
-      <a href="/signout">
-  
+      
+      {/* <p>Enter your personal details for login</p> */}
+      
+      {/* <a href="/signout">
         <button className="border-zinc-500 text-zinc-300 hover:border-zinc-200 hover:text-zinc-200 transition-colors border rounded-full px-8">
           Sign up
         </button>
-  
-      </a>
+      </a> */}
+
     </div>
   </div>
   
   {/* Right Section ----------------------- */}
   <div className="px-4 py-4 my-4 mx-4 md:w-1/2 w-[20rem] bg-white rounded-lg h-dvh basis-[40%]">
-    <h3 className="text-center text-2xl font-semibold">Sign In here</h3>
+    <h3 className="text-center text-2xl font-semibold">Log into Exclusive</h3>
     
     {/* Social Media Sign In Buttons --------------- */}
     <div className="flex justify-center items-center my-3">
@@ -47,7 +48,7 @@ function LoginPage() {
       </button>
     </div>
     
-    <p className="text-center">or use this option</p>
+    <p className="text-center">Enter your details below </p>
     
     {/*  Sign In Form ------------------------- */}
     {/* form onSubmit="onSubmitHandler" */}
@@ -55,14 +56,18 @@ function LoginPage() {
     <form >
       <div className="mb-4">
         <label className="block text-sm font-medium">Email</label>
-        <input type="email" placeholder="Enter your email" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" />
+        <input type="email" placeholder="Email or Phone number" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-medium">Password</label>
-        <input type="password" placeholder="Enter your password" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"/>
+        <input type="password" placeholder="Password" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"/>
       </div>
       
-      <button type="submit" className="w-full mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Login</button>
+      <div  className="w-full max-h-[50px] space-x-20   ">
+      <button type="submit" className="max-w-[150px] max-h-[50px]  mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Login</button>
+      
+      <a href="./signout" className=' text-blue-600 font-Poppins  text-[14]  '> Forget Password? </a>
+      </div>
 
     </form>
   </div>
--- src/app/signout/page.tsx
+++ src/app/signout/page.tsx
@@ -17,23 +17,20 @@ export default function SignOut() {
 
 {/* Signout Start ========================================================== */}
 
-<div className="max-w-[1166px] my-2 mx-auto bg-black flex flex-wrap rounded-lg justify-between items-center">
+<div className="max-w-[1305px] max-h-[780px] my-2 mx-auto   flex flex-wrap rounded-lg justify-between items-center">
+  
   {/* Left Section ------------------------- */}
-  <div className="flex items-center shadow-md rounded-lg overflow-hidden justify-center w-[20rem] mx-6 md:w-auto my-4 basis-[50%]">
-    <div className="text-muted hidden md:flex justify-center items-center flex-col h-dvh w-dvh">
-      <h3 className="text-2xl font-semibold">Already have an account?</h3>
-      <p className="py-4">To keep connected with us please log in with your personal info</p>
-      <a href="/login">
-        <button className="border-zinc-500 text-zinc-300 hover:border-zinc-200 hover:text-zinc-200 transition-colors border rounded-full py-6 px-8">
-          Sign In
-        </button>
-      </a>
+  <div className=" bg-[#CBE4E8] flex items-center shadow-md rounded-lg overflow-hidden justify-center w-[20rem] mx-6 md:w-auto my-4 basis-[50%]">
+    
+    <div className=" text-muted hidden md:flex justify-center items-center flex-col h-dvh w-dvh">
+      
     </div>
+
   </div>
 
   {/* Right Section ----------------------- */}
   <div className="px-4 py-4 my-4 mx-4 md:w-1/2 w-[20rem] bg-white rounded-lg h-dvh basis-[40%]">
-    <h3 className="text-center text-2xl font-semibold">Register Here</h3>
+    <h3 className="text-center text-2xl font-semibold">Create an Account</h3>
 
     {/* Social Media Sign Up Buttons ---------- */}
     <div className="flex justify-center items-center my-3">
@@ -45,30 +42,49 @@ export default function SignOut() {
       </button>
     </div>
 
-    <p className="text-center">or use this option</p>
+    <p className="text-center">Enter your details below</p>
 
     {/* Signup Form ------------------------------ */}
     {/* onsubmit="onSubmitHandler" */}
     <form >
+      
       <div className="mb-4">
         <label className="block text-sm font-medium">Name</label>
         <input type="text" placeholder="Enter your name" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-medium">Email</label>
-        <input type="email" placeholder="Enter your email" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" />
+        <input type="email" placeholder="Email or Phone Number" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-medium">Password</label>
-        <input type="password" placeholder="Enter your password" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" />
+        <input type="password" placeholder="Password" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-medium">Confirm Password</label>
         <input type="password" placeholder="Confirm your password" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500" />
       </div>
-      <button type="submit" className="w-full mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
-        Submit
+
+      <button type="submit" className="w-full mt-4 bg-blue-600 hover:bg-blue-500 text-white font-semibold py-2 px-4 rounded">
+        Create Account
       </button>
+
+      <button type="submit" className="w-full mt-4 bg-blue-200 hover:bg-blue-500 text-black font-semibold py-2 px-4 rounded">
+        Sign up with Google 
+      </button>
+      <br /><br />
+
+
+      <div  className="w-full text-1xl font-semibold ">
+      {/* <div className=" text-muted hidden md:flex justify-center items-center flex-col "> */}
+      Already have an account?
+      <a href="/login"  className=" text-black hover:text-zinc-600 transition-colors tracking-wide " >
+        Login
+      </a>
+      </div>
+        
+
+
     </form>
   </div>
 </div>
